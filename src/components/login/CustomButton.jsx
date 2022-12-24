3ea0e6ef5b4c6c26fb1b26294f358b69fe0cc9fd
dkaodkaork <dkaodkaork@gmail.com>
2022-12-24 20:58:10 +0900

    feat: Button component

---
+++ src/components/login/CustomButton.jsx
@@ -0,0 +1,44 @@
+// 령빈님이랑 상의 해서 button ele로 빼기
+import React from "react"
+import styled from "styled-components"
+
+const CustomButton = ({
+  width,
+  name,
+  height,
+  color,
+  fontSize,
+  fontWeight,
+  backGroundColor,
+}) => {
+  return (
+    <StButton
+      width={width}
+      height={height}
+      color={color}
+      fontSize={fontSize}
+      fontWeight={fontWeight}
+      backGroundColor={backGroundColor}
+    >
+      {name}
+    </StButton>
+  )
+}
+
+export default CustomButton
+
+const StButton = styled.button`
+  border-radius: 0.2rem;
+  border: none;
+  width: ${({ width }) => width};
+  background-color: ${({ backGroundColor }) => backGroundColor};
+  color: ${({ color }) => color};
+  height: ${({ height }) => height};
+  font-size: ${({ fontSize }) => fontSize};
+  font-weight: ${({ fontWeight }) => fontWeight};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  outline: none;
+`
