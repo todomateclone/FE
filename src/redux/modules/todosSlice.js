1b68f3ad6ccab212b43e912976cdcf8eb5ac257f
Dev-RyanK <dev.ryan.kwon@gmail.com>
2022-12-24 23:16:08 +0900

    feat: 전체 목록 관련 모듈 작성

---
+++ src/redux/modules/todosSlice.js
@@ -0,0 +1,76 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { mainApis } from "../../core/api/mainApi"
+import axios from "axios"
+import { serverUrl } from "../../core/api"
+
+const initialState = {
+  allTodos: {},
+  isLoading: false,
+  error: null,
+  msg: "",
+}
+
+export const __getTodos = createAsyncThunk(
+  "todos/get",
+  // async ({ todoYear, todoMonth }, thunkAPI) => {
+  async (payload, thunkAPI) => {
+    try {
+      // const data = await axios.getTodos(
+      //   `${serverUrl}/api/todo/${todoYear}/${todoMonth}`
+      // )
+      const data = await axios.get(`${serverUrl}/data`)
+      // return thunkAPI.fulfillWithValue(data.data.todos)
+      return thunkAPI.fulfillWithValue(data.data)
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err)
+    }
+  }
+)
+
+export const __addTodo = createAsyncThunk(
+  "todos/add",
+  async (tagId, thunkAPI) => {
+    try {
+      await axios.post(`${serverUrl}/api/${tagId}`)
+      return thunkAPI.fulfillWithValue(console.log("success"))
+    } catch (err) {
+      return thunkAPI.rejectWithValue(console.log(err))
+    }
+  }
+)
+
+export const todosSlice = createSlice({
+  name: "allTodos",
+  initialState,
+  reducers: {
+    addTodo: (state, { todoYear, todoMonth }) => {
+      axios.post(`${serverUrl}/todos/${todoYear.payload}/${todoMonth.payload}`)
+      // state.allTodos = action.payload
+    },
+    delTodo: (state, action) => {
+      axios.delete(`${serverUrl}/todos/${action.payload}`)
+      state.allTodos = state.allTodos.filter((v) => v.id !== action.payload)
+    },
+    updateTodo: (state, action) => {
+      axios.patch(`${serverUrl}/todos/${action.payload}`)
+      state.allTodos = action.payload
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(__getTodos.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(__getTodos.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.allTodos = action.payload
+      })
+      .addCase(__getTodos.rejected, (state, action) => {
+        state.isLoading = false
+        state.error = action.payload
+      })
+  },
+})
+
+export const { addTodo, delTodo, updateTodo } = todosSlice.actions
+export default todosSlice.reducer
