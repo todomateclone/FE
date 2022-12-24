e20cbcb583e8a1659582a180a03ee6e895157923
dkaodkaork <dkaodkaork@gmail.com>
2022-12-24 20:57:34 +0900

    feat: Landing page 뼈대 작업

---
+++ src/components/landing/Landing.jsx
@@ -0,0 +1,69 @@
+import React from "react"
+import styled from "styled-components"
+import LoginButton from "../login/CustomButton"
+import { Link } from "react-router-dom"
+
+const Landing = () => {
+  return (
+    <StLoginContainer>
+      <div>
+        <StLandingImg alt="Todomate" src="img/LandingImg.png"></StLandingImg>
+      </div>
+      <div>
+        <div>
+          <h1>todo mate</h1>
+          <p>할 일을 작성, 계획, 관리하세요.</p>
+        </div>
+      </div>
+      <StBtnBox>
+        <StLink to={"/login"}>
+          <LoginButton
+            width="18rem"
+            name="로그인"
+            height="2.3rem"
+          ></LoginButton>
+        </StLink>
+      </StBtnBox>
+      <Link to={"/signup"}>
+        <LinkSignUp>가입하기</LinkSignUp>
+      </Link>
+    </StLoginContainer>
+  )
+}
+
+export default Landing
+
+const StLoginContainer = styled.div`
+  display: flex;
+  height: 50rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  gap: 1rem;
+  h1 {
+    font-weight: 900;
+    font-size: 3rem;
+    margin-bottom: 0rem;
+  }
+  p {
+    margin-top: 0rem;
+    color: #a2a1a1;
+    font-size: 1.1rem;
+  }
+`
+const StBtnBox = styled.div`
+  margin-top: 2rem;
+`
+const StLink = styled(Link)`
+  text-decoration: none;
+`
+const LinkSignUp = styled.div`
+  display: inline;
+  color: black;
+  font-size: 0.9rem;
+`
+const StLandingImg = styled.img`
+  width: 20rem;
+  height: 20rem;
+`
