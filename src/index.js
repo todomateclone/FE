b8863854608120ef715e91e7380ca8fadb8afbf9
Dev-RyanK <dev.ryan.kwon@gmail.com>
2022-12-26 20:40:39 +0900

    feat: bootstrap.css 추가

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import store from "./redux/config/configStore"
 import App from "./App"
+import "bootstrap/dist/css/bootstrap.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
