ff1e055c67d42fbdf73bf2fa19d769848ed9ff7e
Dev-RyanK <dev.ryan.kwon@gmail.com>
2022-12-26 20:37:39 +0900

    feat: 오른편 모달 추가

--- src/styles/assets.js
+++ src/styles/assets.js
@@ -2,4 +2,5 @@ const plusIcon = "/img/todo_plus.webp"
 const pendingIcon = "/img/todo_pending.webp"
 const feedAddBtn = "/img/feedAddBtnIcon.webp"
 const landingImg = "/img/landing.webp"
-export { plusIcon, pendingIcon, landingImg, feedAddBtn }
+const mainSideMenuBtn = "/img/mainSideMenuBtn.webp"
+export { plusIcon, pendingIcon, landingImg, feedAddBtn, mainSideMenuBtn }
