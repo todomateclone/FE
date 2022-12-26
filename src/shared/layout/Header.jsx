0a8433043580fe67f59c15c432c5af6d9254b948
Dev-RyanK <dev.ryan.kwon@gmail.com>
2022-12-26 13:44:32 +0900

    design: border-bottom 추가

--- src/shared/layout/Header.jsx
+++ src/shared/layout/Header.jsx
@@ -12,8 +12,9 @@ const Header = () => {
 export default Header
 
 const StHeader = styled.header`
-  background-color: ${({ theme }) => theme.baseColor.gray};
+  background-color: ${({ theme }) => theme.baseColor.headerGray};
   text-align: right;
   height: 5rem;
   padding: 2rem;
+  border-bottom: 0.1rem solid #f1f1f1;
 `
