c29a0ccf04a36bec3a19a6db53ce4b9dc00ed67a
Dev-RyanK <dev.ryan.kwon@gmail.com>
2022-12-28 06:25:09 +0900

    feat: SubMenuModal 컴포넌트 이동 및 전역 모달 여닫기 기능 slice 추가

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,15 +3,14 @@ import styled from "styled-components"
 import Header from "../../shared/layout/Header"
 import Todolist from "./todolist/Todolist"
 import TodoCalendar from "./calendar/TodoCalendar"
+import SubMenuModal from "../subMenu/SubMenuModal"
 
 const Main = () => {
   return (
     <>
       <Header />
       <StMainWrapper>
-        <StSocial>
-          <h3>소셜</h3>
-        </StSocial>
+        <StSocial></StSocial>
         <StFeed>
           <h3>FEED</h3>
         </StFeed>
@@ -20,6 +19,7 @@ const Main = () => {
         </StCalendar>
         <Todolist />
       </StMainWrapper>
+      <SubMenuModal />
     </>
   )
 }
@@ -27,6 +27,7 @@ const Main = () => {
 export default Main
 
 const StMainWrapper = styled.div`
+  /* position: relative; */
   display: grid;
   grid-template-columns: 22rem 1fr;
   grid-auto-rows: 4rem 1fr;
@@ -35,6 +36,11 @@ const StMainWrapper = styled.div`
     "calendar todolist";
   gap: 2em;
   padding: 2rem 3rem 2rem;
+  overflow-x: hidden;
+  h3 {
+    font-size: xx-large;
+    font-weight: 700;
+  }
 
   @media screen and (max-width: 42.5rem) {
     grid-template-columns: 1fr;
@@ -46,6 +52,7 @@ const StMainWrapper = styled.div`
       "todolist";
     gap: 2em;
     padding: 1.25rem;
+    overflow-x: hidden;
   }
 `
 
