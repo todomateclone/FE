5dd3006481c5ee796e60ca3d6f0619cefb37a6af
Dev-RyanK <dev.ryan.kwon@gmail.com>
2022-12-28 15:05:09 +0900

    design: 우측 모달 디자인 수정

--- src/styles/assets.js
+++ src/styles/assets.js
@@ -3,4 +3,15 @@ const pendingIcon = "/img/todo_pending.webp"
 const feedAddBtn = "/img/feedAddBtnIcon.webp"
 const landingImg = "/img/landing.webp"
 const mainSideMenuBtn = "/img/mainSideMenuBtn.webp"
-export { plusIcon, pendingIcon, landingImg, feedAddBtn, mainSideMenuBtn }
+const settingIcon = "/img/settingIcon.webp"
+const goalRightArrow = "/img/goalRightArrow.webp"
+
+export {
+  plusIcon,
+  pendingIcon,
+  landingImg,
+  feedAddBtn,
+  mainSideMenuBtn,
+  settingIcon,
+  goalRightArrow,
+}
