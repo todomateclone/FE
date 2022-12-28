5d4469b9bfc09b6fe94ae4577cccac6b50dd7344
Dev-RyanK <dev.ryan.kwon@gmail.com>
2022-12-28 09:22:56 +0900

    feat: 할일 CRUD extraReducer 추가

--- src/redux/modules/todosSlice.js
+++ src/redux/modules/todosSlice.js
@@ -1,9 +1,10 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import { baseURL } from "../../core/api/axios"
-import { mainApis } from "../../core/api/mainApi"
+// import { mainApis } from "../../core/api/mainApi"
 
 const initialState = {
   allTodos: {},
+  getTodoId: "",
   isLoading: false,
   error: null,
 }
@@ -21,6 +22,18 @@ export const __getTodos = createAsyncThunk(
   }
 )
 
+export const __getMonthlyTodos = createAsyncThunk(
+  "todos/getMonthly",
+  async ({ todoYear, todoMonth }, thunkAPI) => {
+    try {
+      const data = await baseURL.get(`/todo/${todoYear}/${todoMonth}`)
+      return thunkAPI.fulfillWithValue(data.data)
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err)
+    }
+  }
+)
+
 export const __addTodo = createAsyncThunk(
   "todos/add",
   async ({ tagId, content }, thunkAPI) => {
@@ -28,6 +41,30 @@ export const __addTodo = createAsyncThunk(
       // 이걸 쓰면 터진다. 왤까...
       // await mainApis.postTodo(tagId, content)
       await baseURL.post(`/todo/${tagId}`, content)
+      return thunkAPI.fulfillWithValue(console.log("success"))
+    } catch (err) {
+      return thunkAPI.rejectWithValue(console.log(err))
+    }
+  }
+)
+
+export const __delTodo = createAsyncThunk(
+  "todos/del",
+  async (todoId, thunkAPI) => {
+    try {
+      await baseURL.delete(`/todo/${todoId}`)
+      return thunkAPI.fulfillWithValue(console.log("success"))
+    } catch (err) {
+      return thunkAPI.rejectWithValue(console.log(err))
+    }
+  }
+)
+
+export const __putTodo = createAsyncThunk(
+  "todos/put",
+  async ({ todoId, content }, thunkAPI) => {
+    try {
+      await baseURL.put(`/todo/${todoId}`, content)
       return thunkAPI.fulfillWithValue(console.log("success"))
     } catch (err) {
       return thunkAPI.rejectWithValue(console.log(err))
@@ -51,15 +88,19 @@ export const todosSlice = createSlice({
     },
     delTodo: (state, action) => {
       baseURL.delete(`/todo/${action.payload}`)
-      state.allTodos = state.allTodos.filter((v) => v.id !== action.payload)
+      state.allTodos = state.allTodos.filter((v) => v.todoId !== action.payload)
     },
     updateTodo: (state, action) => {
       baseURL.patch(`/todo/${action.payload}`)
       state.allTodos = action.payload
     },
+    sendTodoId: (state, action) => {
+      state.getTodoId = action.payload
+    },
   },
   extraReducers: (builder) => {
     builder
+      // 기본 조회
       .addCase(__getTodos.pending, (state) => {
         state.isLoading = true
       })
@@ -71,6 +112,19 @@ export const todosSlice = createSlice({
         state.isLoading = false
         state.error = action.payload
       })
+      // 월간 조회
+      .addCase(__getMonthlyTodos.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(__getMonthlyTodos.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.allTodos = action.payload
+      })
+      .addCase(__getMonthlyTodos.rejected, (state, action) => {
+        state.isLoading = false
+        state.error = action.payload
+      })
+      // 등록
       .addCase(__addTodo.pending, (state) => {
         state.isLoading = true
       })
@@ -82,8 +136,38 @@ export const todosSlice = createSlice({
         state.isLoading = false
         state.error = action.payload
       })
+      // 삭제
+      .addCase(__delTodo.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(__delTodo.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.allTodos.data.todos = state.allTodos.data.todos.filter(
+          (item) => item.todoId !== action.payload
+        )
+      })
+      .addCase(__delTodo.rejected, (state, action) => {
+        state.isLoading = false
+        state.error = action.payload
+      })
+      // 수정
+      .addCase(__putTodo.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(__putTodo.fulfilled, (state, action) => {
+        const idx = state.findIndex((todo) => todo.id === action.payload.id)
+        state[idx] = {
+          ...state[idx],
+          ...action.payload,
+        }
+      })
+      .addCase(__putTodo.rejected, (state, action) => {
+        state.isLoading = false
+        state.error = action.payload
+      })
   },
 })
 
-export const { addTodo, delTodo, updateTodo } = todosSlice.actions
+export const { addTodo, delTodo, updateTodo, sendTodoId, getTodoId } =
+  todosSlice.actions
 export default todosSlice.reducer
