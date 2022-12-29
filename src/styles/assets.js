72a656e087eed4697d573ce8932d0b37a1a2a4b1
dkaodkaork <dkaodkaork@gmail.com>
2022-12-29 10:23:31 +0900

    design: 이미지 파일 추가

--- src/styles/assets.js
+++ src/styles/assets.js
@@ -5,6 +5,10 @@ const landingImg = "/img/landing.webp"
 const mainSideMenuBtn = "/img/mainSideMenuBtn.webp"
 const settingIcon = "/img/settingIcon.webp"
 const goalRightArrow = "/img/goalRightArrow.webp"
+const goalSeeOnlyIcon = "/img/goalSeeonlymeIcon.webp"
+const goalHideIcon = "/img/goalHideIcon_1.webp"
+const goalPartialdisclosureIcon = "/img/goalPartialdisclosureIcon_1.webp"
+const goalFulldisclosureIcon = "/img/goalFulldisclosureIcon_1.webp"
 
 export {
   plusIcon,
@@ -14,4 +18,8 @@ export {
   mainSideMenuBtn,
   settingIcon,
   goalRightArrow,
+  goalSeeOnlyIcon,
+  goalHideIcon,
+  goalPartialdisclosureIcon,
+  goalFulldisclosureIcon,
 }
